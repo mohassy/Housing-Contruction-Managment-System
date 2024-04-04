72bda394b84555602ff648abc5dadda20184e8b3
cattiestsine75 <ethan.zelmer@gmail.com>
2024-04-03 21:48:52 -0400

    removed additional endpoint

--- master-microservice/main.py
+++ master-microservice/main.py
@@ -67,42 +67,6 @@ def get_rank(locations: List[str]):
 
 
 
-@app.post("/rank", response_model=List[str])
-def get_rank(locations: List[str]):
-    # values microservice ranking (Ethan)
-    nlocs = ""
-    for location in locations:
-         nlocs = location + "," + nlocs
-    with grpc.insecure_channel('localhost:50051') as channel:
-            stub = valueMS_pb2_grpc.valueMicroserviceStub(channel)
-            try:
-                print("Retrieving the response from a serialReq")
-                response = stub.getEnv(valueMS_pb2.rankreq(locations=nlocs))
-                #print("response:" + str(response))
-                print("Hm?")
-                print("RANKING:" + response.rankings)
-                print("DONE RANKING")
-            except grpc.RpcError as e:
-                print("Error: " , e.details())
-    # policy microservice ranking (Astha)
-
-    LMS_locs = ','.join(locations)   # covert list to a string
-    with grpc.insecure_channel('localhost:50051') as channel:
-        LMS_stub = legalMicroservice_pb2_grpc.legalMicroserviceStub(channel)
-        try:
-            LMS_response_str = LMS_stub.getLocations(legalMicroservice_pb2.Locations(locations=LMS_locs))
-            # convert response (string) into an array
-            LMS_response = LMS_response_str.split(',')  # expected: 1D array of available locations
-        except grpc.RpcError as e:
-            if e.code() == grpc.StatusCode.NOT_FOUND:
-                raise HTTPException(status_code=404, detail='Response not found')
-
-    # values microservice ranking (Shadman)
-
-    # combine rankings (Hassan)
-    print(locations)
-    return sorted(locations)
-
 
 load_dotenv(dotenv_path='./app/config/.env')
 
