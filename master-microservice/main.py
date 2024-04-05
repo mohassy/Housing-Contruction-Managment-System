b9e0cd37ad262e01c261cf8d30e3ee45ea21be8d
mohassy <mohammed.siadhassy@gmail.com>
2024-04-04 22:48:33 -0400

    ranked endpoint finally outputs the correct rank and the frontend works with the results as expected

--- master-microservice/main.py
+++ master-microservice/main.py
@@ -1,5 +1,6 @@
+import ast
 from typing import List
-
+import json
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,11 +9,14 @@ import grpc
 import sys
 
 import os
+
 sys.path.append("../value-microservice/")
 from app.controllers import project_controller, stakeholder_controller, task_controller, post_controller
-import legalMicroservice_pb2, legalMicroservice_pb2_grpc, valueMS_pb2_grpc, valueMS_pb2, proximityMicroservice_pb2_grpc, proximityMicroservice_pb2
+import legalMicroservice_pb2, legalMicroservice_pb2_grpc, valueMS_pb2_grpc, valueMS_pb2, proximityMicroservice_pb2_grpc, \
+    proximityMicroservice_pb2
 import valueMS_pb2_grpc
 import valueMS_pb2
+
 sys.path.append("../proximity-microservice/")
 sys.path.append("../legalMicroservice/")
 import proximityMicroservice_pb2_grpc
@@ -42,18 +46,18 @@ def get_rank(locations: List[str]):
     # values microservice ranking (Ethan)
     nlocs = ""
     for location in locations:
-         nlocs = location + "," + nlocs
+        nlocs = location + "," + nlocs
     with grpc.insecure_channel('localhost:50051') as channel:
-            stub = valueMS_pb2_grpc.valueMicroserviceStub(channel)
-            try:
-                print("Retrieving the response from a getEnvironment")
-                response = stub.getEnv(valueMS_pb2.rankreq(locations=nlocs))
-                #print("response:" + str(response))
-                print("Hm?")
-                print("RANKING:" + response.rankings)
-                print("DONE RANKING")
-            except grpc.RpcError as e:
-                print("Error: " , e.details())
+        stub = valueMS_pb2_grpc.valueMicroserviceStub(channel)
+        try:
+            print("Retrieving the response from a getEnvironment")
+            response = stub.getEnv(valueMS_pb2.rankreq(locations=nlocs))
+            # print("response:" + str(response))
+            print("Hm?")
+            print("RANKING:" + response.rankings)
+            print("DONE RANKING")
+        except grpc.RpcError as e:
+            print("Error: ", e.details())
     # policy microservice ranking (Astha)
     with grpc.insecure_channel('localhost:50052') as channel:
         LMS_stub = legalMicroservice_pb2_grpc.legalMicroserviceStub(channel)
@@ -61,7 +65,7 @@ def get_rank(locations: List[str]):
             LMS_response_str = LMS_stub.getLocations(legalMicroservice_pb2.Locations(locations=nlocs))
             # convert response (string) into an array
             LMS_response = LMS_response_str.status.split(',')  # expected: 1D array of available locations
-            print(LMS_response)
+            print("legal locations: "+str(LMS_response))
         except grpc.RpcError as e:
             if e.code() == grpc.StatusCode.NOT_FOUND:
                 raise HTTPException(status_code=404, detail='Response not found')
@@ -78,11 +82,29 @@ def get_rank(locations: List[str]):
         except grpc.RpcError as e:
             print("Error: ", e.details())
 
-    # combine rankings (Hassan)
+    # Convert the string into an actual list of tuples
+    data_list1 = ast.literal_eval(response.rankings)
 
-    return sorted(locations)
+    data_list2 = ast.literal_eval(proxResponse.rankedAreas)
 
+    combined_tuples = []
 
+    for (location1, value1), (location2, value2) in zip(data_list1, data_list2):
+        combined_value = value1 + value2
+        combined_tuples.append((location1, combined_value))
+
+    # Sort the combined_tuples based on the combined values in ascending order
+    sorted_combined_tuples = sorted(combined_tuples, key=lambda x: x[1])
+    print("sorted(ASC) combined tuples: " + str(sorted_combined_tuples))
+    # Extract the locations from the sorted_combined_tuples in descending order
+    sorted_locations = [location for location, _ in sorted_combined_tuples[::-1]]
+    print("sorted locations: " + str(sorted_locations))
+
+    for location in sorted_locations:
+        if LMS_response.count(location) == 0:
+            sorted_locations.remove(location)
+    print("sorted + removed illegal locations: " + str(sorted_locations))
+    return sorted_locations
 
 
 load_dotenv(dotenv_path='./app/config/.env')
